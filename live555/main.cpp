e50fdd7b6e27f004307362bb02ce2ce71e58ed28
zer0 <osom8979@gmail.com>
2016-02-04 16:48:21 +0900

    Create a live555 example project.

---
+++ live555/main.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+
+#include <liveMedia/liveMedia.hh>
+#include <BasicUsageEnvironment/BasicUsageEnvironment.hh>
+
+using namespace std;
+
+int main(int argc, char ** argv)
+{
+    cout << "Hello, world!\n";
+    return 0;
+}
+
