473d8817d17f6f826e2368f12c4b46217a2cc209
zer0 <osom8979@gmail.com>
2016-02-25 10:46:38 +0900

    Refactoring build script.

--- template/cmake-proj/cmake/protobuf.cmake
+++ template/cmake-proj/cmake/protobuf.cmake
@@ -5,36 +5,49 @@
 # \param _working      [in] working directory.
 # \param _prefix       [in] install prefix directory.
 # \param _include_path [in] CPP, include path.
-# \param _library_parh [in] LINK, library path.
+# \param _library_path [in] LINK, library path.
 function (install_protobuf _working _prefix _include_path _library_path)
 
-    set (_proj_name    "protobuf")
-    set (_proj_url     "https://github.com/google/protobuf.git")
-    set (_parent_dir   "${_working}")
-    set (_build_dir    "${_parent_dir}/${_proj_name}")
-    set (_prefix_dir   "${_prefix}")
-    set (_include_dir  "${_include_path}")
-    set (_library_dir  "${_library_path}")
+    set (_proj_name "protobuf")
+    set (_proj_url  "https://github.com/google/protobuf.git")
+    set (_build_dir "${_working}/${_proj_name}")
 
-    # Download.
+    if (NOT EXISTS "${_build_dir}")
+        message ("** Download ${_proj_name}")
+        execute_process (
+            COMMAND git clone --depth=1 ${_proj_url}
+            WORKING_DIRECTORY "${_working}"
+        )
+    endif ()
+
+    if (NOT EXISTS "${_build_dir}/configure")
+        message ("** Autogen ${_proj_name}")
+        execute_process (
+            COMMAND ./autogen.sh
+            WORKING_DIRECTORY "${_build_dir}"
+        )
+    endif ()
+
+    message ("** Configure ${_proj_name}")
     execute_process (
-        COMMAND git clone --depth=1 ${_proj_url}
-        WORKING_DIRECTORY "${_parent_dir}"
+        COMMAND ./configure --disable-shared --enable-static
+                            --prefix=${_prefix}
+                            #CPPFLAGS=-I${_include_path}
+                            #LDFLAGS=-L${_library_path}
+        WORKING_DIRECTORY "${_build_dir}"
     )
 
-    # Build & install.
+    include (ProcessorCount)
+    ProcessorCount (_process_count)
+    math (EXPR _thread_count "${_process_count} * 2")
+
+    message ("** Build ${_proj_name}")
     execute_process (
-        COMMAND ./autogen.sh
+        COMMAND make -j${_thread_count} V=0
         WORKING_DIRECTORY "${_build_dir}"
     )
-    execute_process (
-        COMMAND ./configure --prefix=${_prefix_dir} CPPFLAGS=-I${_include_dir} LDFLAGS=-L${_library_dir}
-        WORKING_DIRECTORY "${_build_dir}"
-    )
-    execute_process (
-        COMMAND make
-        WORKING_DIRECTORY "${_build_dir}"
-    )
+
+    message ("** Install ${_proj_name}")
     execute_process (
         COMMAND make install
         WORKING_DIRECTORY "${_build_dir}"
--- template/cmake-proj/cmake/zlib.cmake
+++ template/cmake-proj/cmake/zlib.cmake
@@ -4,36 +4,42 @@
 #
 # \param _working      [in] working directory.
 # \param _prefix       [in] install prefix directory.
-# \param _include_path [in] CPP, include path.
-# \param _library_parh [in] LINK, library path.
-function (install_zlib _working _prefix _include_path _library_path)
+function (install_zlib _working _prefix)
 
     set (_proj_name     "zlib-1.2.8")
     set (_proj_url      "http://zlib.net/zlib-1.2.8.tar.gz")
-    set (_parent_dir    "${_working}")
-    set (_download_file "${_parent_dir}/${_proj_name}.tar.gz")
-    set (_build_dir     "${_parent_dir}/${_proj_name}")
-    set (_prefix_dir    "${_prefix}")
-    set (_include_dir   "${_include_path}")
-    set (_library_dir   "${_library_path}")
+    set (_download_file "${_working}/${_proj_name}.tar.gz")
+    set (_build_dir     "${_working}/${_proj_name}")
 
-    # Download & extract.
+    if (NOT EXISTS "${_download_file}")
+        message ("** Download ${_proj_name}")
+        execute_process (
+            COMMAND curl -o "${_download_file}" "${_proj_url}"
+            WORKING_DIRECTORY "${_working}"
+        )
+    endif ()
+
+    if (NOT EXISTS "${_build_dir}")
+        message ("** Extract ${_proj_name}")
+        execute_process (
+            COMMAND tar vxzf "${_download_file}"
+            WORKING_DIRECTORY "${_working}"
+        )
+    endif ()
+
+    message ("** Configure ${_proj_name}")
     execute_process (
-        COMMAND curl -o "${_download_file}" "${_proj_url}"
-        WORKING_DIRECTORY "${_parent_dir}"
-    )
-    execute_process (
-        COMMAND tar vxzf "${_download_file}"
-        WORKING_DIRECTORY "${_parent_dir}"
-    )
-    execute_process (
-        COMMAND ./configure --prefix=${_prefix_dir}
+        COMMAND ./configure --prefix=${_prefix}
         WORKING_DIRECTORY "${_build_dir}"
     )
+
+    message ("** Build ${_proj_name}")
     execute_process (
         COMMAND make
         WORKING_DIRECTORY "${_build_dir}"
     )
+
+    message ("** Install ${_proj_name}")
     execute_process (
         COMMAND make install
         WORKING_DIRECTORY "${_build_dir}"
