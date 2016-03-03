e2892659d8c19c31f32f9427f094618359b3184d
zer0 <osom8979@gmail.com>
2016-03-03 10:04:16 +0900

    link.cmake merge to compile.cmake

--- template/cmake-proj/cmake/compile.cmake
+++ template/cmake-proj/cmake/compile.cmake
@@ -1,5 +1,19 @@
 # CMake compile utility script.
 
+#! Insert whole-archive flags.
+#
+# \param _value [out] value name of output list.
+macro (insert_whole_archive_flags _value)
+    #message ("_value: ${_value} (${${_value}})")
+
+    if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
+        list (INSERT ${_value} 0 "-Wl,-force_load")
+    elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
+        list (INSERT ${_value} 0 "-Wl,--whole-archive")
+        list (APPEND ${_value} "-Wl,--no-whole-archive")
+    endif ()
+endmacro ()
+
 #! find object & library files.
 #
 # \param _objs      [out] value name of result object files.
