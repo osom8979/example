f15176a089a4acdc6cabbd15aac94491c87d704f
zer0 <osom8979@gmail.com>
2016-03-05 09:29:10 +0900

    Add a lmdb build script.

--- template/cmake-proj/config.cmake
+++ template/cmake-proj/config.cmake
@@ -26,6 +26,7 @@ add_library_value ("protobuf"  ON  LIST_OF_LIBRARIES)
 add_library_value ("gflags"    ON  LIST_OF_LIBRARIES)
 add_library_value ("glog"      ON  LIST_OF_LIBRARIES)
 add_library_value ("boost"     ON  LIST_OF_LIBRARIES)
+add_library_value ("lmdb"      ON  LIST_OF_LIBRARIES)
 set (LIST_OF_LIBRARIES ${LIST_OF_LIBRARIES})
 
 # Main version.
