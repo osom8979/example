fcd56b03444607ef1c7bf662ceecdf392868bc0f
zer0 <osom8979@gmail.com>
2016-03-06 14:26:37 +0900

    Add a snappy build script.

--- template/cmake-proj/config.cmake
+++ template/cmake-proj/config.cmake
@@ -30,6 +30,7 @@ add_library_value ("lmdb"      ON  LIST_OF_LIBRARIES)
 add_library_value ("hdf5"      ON  LIST_OF_LIBRARIES)
 add_library_value ("openblas"  ON  LIST_OF_LIBRARIES)
 add_library_value ("libuv"     ON  LIST_OF_LIBRARIES)
+add_library_value ("snappy"    ON  LIST_OF_LIBRARIES)
 set (LIST_OF_LIBRARIES ${LIST_OF_LIBRARIES})
 
 # Main version.
