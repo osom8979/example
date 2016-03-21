5846cd0fd20db14b1958dc8e35db16ae9ac40fb0
zer0 <osom8979@gmail.com>
2016-03-21 16:13:02 +0900

    Remove unused files.

--- template/cmake-proj/cmake/libraries/leveldb-install.cmake
+++
@@ -1,9 +0,0 @@
-## LevelDB library install script.
-#
-# _build_dir
-# _prefix
-# _static_lib_name
-
-execute_process (COMMAND ${CMAKE_COMMAND} -E copy "${_build_dir}/${_static_lib_name}"  "${_prefix}/lib/${_static_lib_name}")
-execute_process (COMMAND ${CMAKE_COMMAND} -E copy_directory "${_build_dir}/include"    "${_prefix}/include")
-
