fc110fb9bb95d83fd6b2588b2f3e3b76e0834a19
zer0 <osom8979@gmail.com>
2016-03-26 16:33:31 +0900

    Fix the wxWidgets include_dir bug.

--- template/cmake-proj/cmake/util.cmake
+++ template/cmake-proj/cmake/util.cmake
@@ -81,3 +81,17 @@ function (list_regex _value _regex _list)
     set (${_value} ${${_value}} PARENT_SCOPE)
 endfunction ()
 
+#! Convert msys path to windows path.
+#
+# \param _value     [out] output value name.
+# \param _path_list [in]  MSYS path list.
+function (convert_msys_to_windows_path _value _path_list)
+    set (${_value})
+    foreach (_cursor ${_path_list})
+        string (REGEX REPLACE "^/([a-zA-Z])/" "\\1:/" _replace_cursor1 ${_cursor})
+        string (REGEX REPLACE "/+" "\\\\" _replace_cursor2 ${_replace_cursor1})
+        list (APPEND ${_value} ${_replace_cursor2})
+    endforeach ()
+    set (${_value} ${${_value}} PARENT_SCOPE)
+endfunction ()
+
