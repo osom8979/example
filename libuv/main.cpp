fe2e81a7dd31faf7d08a50e3d51284e28020a56b
zer0 <osom8979@gmail.com>
2016-04-01 17:05:15 +0900

    libuv: First Event loop.

--- libuv/main.cpp
+++ libuv/main.cpp
@@ -1,11 +1,30 @@
 
 #include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+#include <uv.h>
 
 using namespace std;
 
+// First Event loop.
+int event_loop()
+{
+    uv_loop_t * loop = (uv_loop_t *) malloc(sizeof(uv_loop_t));
+    uv_loop_init(loop);
+
+    printf("Now quitting.\n");
+    uv_run(loop, UV_RUN_DEFAULT);
+
+    uv_loop_close(loop);
+    free(loop);
+
+    return 0;
+}
+
 int main(int argc, char ** argv)
 {
-    cout << "Hello, world!\n";
-    return 0;
+    return event_loop();
 }
 
