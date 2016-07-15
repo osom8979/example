06252fea728a4a07dfa6f49e88d77fec7d088d89
zer0 <osom8979@gmail.com>
2016-07-15 13:04:54 +0900

    Create default opencv template.

---
+++ opencv/template/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+#include <opencv2/opencv.hpp>
+
+int main(int argc, char ** argv)
+{
+    cv::VideoCapture cap(0);
+    if (cap.isOpened() == 0) {
+        return 1;
+    }
+
+    cv::Mat frame;
+    cv::Mat preview;
+
+    int const VKEY_ESC = 30;
+    std::string const TITLE = "OpenCV Sample";
+    cv::namedWindow(TITLE);
+
+    while (true) {
+        cap >> frame;
+
+        // PREVIEW CODE.
+        preview = frame;
+
+        cv::imshow(TITLE, preview);
+        if (cv::waitKey(VKEY_ESC) >= 0) {
+            break;
+        }
+    }
+
+    return 0;
+}
+
